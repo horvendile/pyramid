8404fcf0c7549d05fa7071b7de40b16fb18bebb1
horvendile <horvendile@montfalcon.co.uk>
2018-01-08 13:50:06 +0000

    Test

--- contract.sol
+++ contract.sol
@@ -1,9 +1,10 @@
 pragma solidity ^0.4.11;
+
 contract TheButton {
     uint public lastPressed;
     event ButtonPress(address presser, uint pressTime, uint score);
-    
-    /// @notice Press the button! 
+
+    /// @notice Press the button!
     function press() {
         ButtonPress(msg.sender, now, (now - lastPressed)%256);
         lastPressed = now;
