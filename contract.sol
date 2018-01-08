5a6c7323b71b97903c11e316e29df9fa84833c4d
horvendile <horvendile@montfalcon.co.uk>
2018-01-08 14:04:18 +0000

    Test

--- contract.sol
+++ contract.sol
@@ -4,7 +4,7 @@ contract TheButton {
     uint public lastPressed;
     event ButtonPress(address presser, uint pressTime, uint score);
 
-    /// @notice Press the button!
+    /// @notice Press the button!!!!
     function press() {
         ButtonPress(msg.sender, now, (now - lastPressed)%256);
         lastPressed = now;
